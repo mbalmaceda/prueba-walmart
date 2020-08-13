1bf60f329ff4eae7b0d87397419d447c685df83f
Mirko Balmaceda <mfbalmacedam@MACBOOKPRO-MFBALMACEDAM.local>
2020-08-12 22:24:20 -0400

    Initial commit

---
+++ search-product-ms/src/test/java/com/walmart/lider/SearchProductMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.lider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchProductMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
